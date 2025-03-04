8631eef72ffa932f4b6f10c963a011c0c748ffc1
TreesaKuriakose <kuriakosetreesa28@gmail.com>
2025-03-04 10:43:25 +0530

    part 8

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('my-url/<fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
+
+from django.shortcuts import get_object_or_404
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    return render(request, 'catalog/book_detail.html', context={'book': book})
+
+def my_view(request, fish):
+    """View function for my_url."""
+    return render(request, 'some_path.html', context={'fish': fish})
+
+
+
+
